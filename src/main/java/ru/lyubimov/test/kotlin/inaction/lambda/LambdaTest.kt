b33c9580dc5700e993101988c77b140fd2cf0f35
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-07-31 22:51:29 +0300

    KiA 150 pg, p5.2

--- src/main/java/ru/lyubimov/test/kotlin/inaction/lambda/LambdaTest.kt
+++ src/main/java/ru/lyubimov/test/kotlin/inaction/lambda/LambdaTest.kt
@@ -1,12 +1,28 @@
 package ru.lyubimov.test.kotlin.inaction.lambda
 
+fun main(args: Array<String>) {
+    val errors = listOf("403 Forbidden", "404 Not Found")
+    printMessagesWithPrefix(errors, "Error:")
+
+    val responces = listOf("200 OK", "418 I'm a teapot", "500 Internal Server Error")
+    printProblemCounts(responces)
+}
+
 fun printMessagesWithPrefix(messages: Collection<String>, prefix : String) {
     messages.forEach {
         println("$prefix $it")
     }
 }
 
-fun main(args: Array<String>) {
-    val errors = listOf("403 Forbidden", "404 Not Found")
-    printMessagesWithPrefix(errors, "Error:")
+fun printProblemCounts(responces : Collection<String>) {
+    var clientErrors = 0
+    var serverErrors = 0
+    responces.forEach{
+        if(it.startsWith("4")){
+            clientErrors++
+        } else if(it.startsWith("5")) {
+            serverErrors++
+        }
+    }
+    println("$clientErrors client errors, $serverErrors server errors")
 }
