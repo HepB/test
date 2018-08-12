8cd49aac43e9796dc3b55f6ffd2bd73a0387b8ef
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-08-12 23:18:01 +0300

    Added codewars package, changed pom.xml

---
+++ src/main/java/ru/lyubimov/test/codewars/FindOdd.java
@@ -0,0 +1,31 @@
+package ru.lyubimov.test.codewars;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class FindOdd {
+    static int findIt(int[] a) {
+        HashMap<Integer, Integer> pairs = new HashMap<>();
+        int result = 0;
+        for (int element : a) {
+            Integer mapElem = pairs.get(element);
+            if (mapElem == null) {
+                pairs.put(element, 1);
+            } else {
+                pairs.replace(element, ++mapElem);
+            }
+        }
+        for(Map.Entry<Integer, Integer> entry : pairs.entrySet()) {
+            int entryCount =  entry.getValue();
+            if(entryCount % 2 != 0) {
+                result = entry.getKey();
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        findIt(new int[]{20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5});
+    }
+}
