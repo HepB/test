332c47bd1f3e49c44116be1e9e1f953bf9de2932
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-08-12 21:43:11 +0300

    KiA 296 pg, p9.3

---
+++ src/main/java/ru/lyubimov/test/kotlin/inaction/generics/GenericTest.kt
@@ -0,0 +1,8 @@
+package ru.lyubimov.test.kotlin.inaction.generics
+
+val <T> List<T>.penultimate: T
+    get() = this[size - 2]
+
+fun main(args: Array<String>) {
+    println(listOf(1, 2, 3, 4).penultimate)
+}
