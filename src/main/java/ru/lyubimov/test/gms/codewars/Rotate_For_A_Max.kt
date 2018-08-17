f91bd851d871c5c31d30a7ff9523668b7450668e
HepBo3 <dr_kuvento@mail.ru>
2018-08-17 16:58:47 +0300

    codewars: Rotate for a max. fix

--- src/main/java/ru/lyubimov/test/gms/codewars/Rotate_For_A_Max.kt
+++ src/main/java/ru/lyubimov/test/gms/codewars/Rotate_For_A_Max.kt
@@ -1,11 +1,29 @@
 package ru.lyubimov.test.gms.codewars
 
 fun maxRot(n: Long): Long {
-    val sb = StringBuilder()
-    n.toString().toMutableList().apply { sort() }.apply { reverse() }.forEach{sb.append(it)}
-    return sb.toString().toLong()
+    val array = n.toString().toMutableList()
+    val variants: MutableList<Long> = mutableListOf()
+    with(array) {
+        for (i in 0 until size) {
+            val leftElement = array.removeAt(i)
+            add(leftElement)
+            variants.add(joinToString(separator = "", prefix = "", postfix = "").toLong())
+        }
+    }
+    return variants.max() ?: n
+}
+
+fun MutableList<Char>.moveLeft(): MutableList<Char> {
+    val leftElement = removeAt(0)
+    add(leftElement)
+    return this
 }
 
 fun main(args: Array<String>) {
-    println(maxRot(3413983))
+    println(maxRot(38458215))
+    println(maxRot(3333))
+    println(1)
+    val char = mutableListOf('a', 'b', 'c', 'd')
+    println(char.moveLeft().moveLeft().moveLeft().moveLeft())
+    println(char)
 }
