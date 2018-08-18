6cb592100055322056969eeee2c492db479a7923
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-08-18 19:04:34 +0300

    codewars: The Walker. Added solution class

---
+++ src/main/java/ru/lyubimov/test/gms/codewars/The_Walker.kt
@@ -0,0 +1,9 @@
+package ru.lyubimov.test.gms.codewars
+
+fun solve(a:Int, b:Int, c:Int, alpha:Int, beta:Int, gamma:Int):IntArray {
+    // your code
+    val result = IntArray(4)
+
+
+    return result
+}
