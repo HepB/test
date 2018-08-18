ab149b7ebf8443e726868e409e34fd6a79a0687c
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-08-18 19:15:54 +0300

    CiA 325, p10.1.7

--- src/main/java/ru/lyubimov/test/kotlin/inaction/jkid/Person.kt
+++ src/main/java/ru/lyubimov/test/kotlin/inaction/jkid/Person.kt
@@ -1,10 +1,15 @@
 package ru.lyubimov.test.kotlin.inaction.jkid
 
+import ru.lyubimov.test.kotlin.inaction.jkid.annotation.CustomSerializer
 import ru.lyubimov.test.kotlin.inaction.jkid.annotation.DeserializeInterface
 import ru.lyubimov.test.kotlin.inaction.jkid.annotation.JsonExclude
 import ru.lyubimov.test.kotlin.inaction.jkid.annotation.JsonName
+import ru.lyubimov.test.kotlin.inaction.jkid.serializer.DataSerializer
+import java.util.*
 
 data class Person(
         @JsonName("alias") val name: String,
         @JsonExclude val age: Int? = null,
-        @DeserializeInterface(CompanyImpl::class) val company: Company)
+        @DeserializeInterface(CompanyImpl::class) val company: Company,
+        @CustomSerializer(DataSerializer::class) val birthDate: Date
+)
