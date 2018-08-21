907bc3cc2342707bb3ff337d871a2413cc37cf3e
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-08-21 14:28:18 +0300

    codewars: Do multiple replacment. Trying to resolve.

--- src/main/java/ru/lyubimov/test/gms/codewars/Do_Multiple_Replacment.kt
+++ src/main/java/ru/lyubimov/test/gms/codewars/Do_Multiple_Replacment.kt
@@ -1,13 +1,17 @@
 package ru.lyubimov.test.gms.codewars
 
-fun f(s: String): String = Regex("""[{\[(>})<\]]""").replace(s){c->when(c.value[0]){'{','[','<'->(c.value[0] + 2).toString()'(' -> ")"')' -> "(" else -> (c.value[0] - 2).toString()}}
-fun f1(s: String): String = s.map{when(it){'<','{','['->it+2 '>','}',']'->it-2 '('->')'')'->'(' else->it}}.joinToString("")
-//fun f2(s: String): String = s.map{when(it){'<','{','['->it+2 '>','}',']'->it-2 '('->')'')'->'(' else->it}}
+fun f(s: String): String = Regex("""[{\[(>})<\]]""").replace(s){c -> when( c.value[0] ){'{','[','<'->(c.value[0] + 2).toString()'(' -> ")"')' -> "(" else -> (c.value[0] - 2).toString()}}
+fun f1(s: String) = s.map { when(it) { '<','{','[' ->it + 2 '>','}',']'->it - 2 '('->')'')'->'(' else -> it}}.joinToString("")
+fun f2(s: String) = s.replaceIndent("""""")
+//fun f2(s: String) = s.replace()
 
-val c = """s.map{when(it){'<','{','['->it+2 '>','}',']'->it-2 '('->')'')'->'(' else->it}}.joinToString("")"""
+val c = """fun f1(s: String) = s.map { when(it) { '<','{','[' ->it + 2 '>','}',']'->it-2 '('->')'')'->'(' else->it}}.joinToString("")"""
 fun main(args: Array<String>) {
     println(f("dfs}sdf]("))
     println(f1("dfs}sdf]("))
-    //println(f2("dfs}sdf]("))
-    println(c.length)
+    println(f2("dfs}sdf](){["))
+    println(c.lengthOnlyChars())
 }
+
+fun String.lengthOnlyChars(): Int =
+        this.replace(" ","").length
